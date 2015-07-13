cffa70bc7096605798b4308ece05f447d0e90dea
vickielawlor <112431052@umail.ucc.ie>
2015-07-13 12:57:23 +0100

    Update myapp.rb

--- myapp.rb
+++ myapp.rb
@@ -46,7 +46,7 @@ class MyApp < Sinatra::Base
     logger.info "Visited home page"
      @accounts1 = client.query("SELECT FirstName,LastName,MobilePhone,Email FROM User WHERE Id = '00580000003lR2a' OR Id = '00580000003lR5B'
                     OR Id = '00580000003lQuG' OR Id = '005340000082AzV' OR Id = '00580000003muAa' ")  
-     @accounts2 = clientt.query("SELECT owner.name from event where owner.title in ('cloud services engineer','cloud service engineer')")
+     @accounts2 = clientt.query("SELECT owner.name from event where owner.title in ('cloud services engineer','cloud service engineer') group by owner.name")
     erb :index
   end
 
