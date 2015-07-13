0385f6c186d31c3b9b8b8f69f61233a3743612d2
vickielawlor <112431052@umail.ucc.ie>
2015-07-13 12:52:18 +0100

    Update myapp.rb

--- myapp.rb
+++ myapp.rb
@@ -30,6 +30,14 @@ class MyApp < Sinatra::Base
                             client_id:     ENV['SALESFORCE_KEY'],
                             client_secret: ENV['SALESFORCE_SECRET']
     end
+    
+     def clientt
+      @client ||= Force.new instance_url:  session['instance_url'], 
+                            oauth_token:   session['token'],
+                            refresh_token: session['refresh_token'],
+                            client_id:     ENV['SALESFORCE_KEY'],
+                            client_secret: ENV['SALESFORCE_SECRET']
+    end
 
   end
 
@@ -38,6 +46,7 @@ class MyApp < Sinatra::Base
     logger.info "Visited home page"
      @accounts1 = client.query("SELECT FirstName,LastName,MobilePhone,Email FROM User WHERE Id = '00580000003lR2a' OR Id = '00580000003lR5B'
                     OR Id = '00580000003lQuG' OR Id = '005340000082AzV' OR Id = '00580000003muAa' ")  
+     @accounts2 = clientt.query("SELECT owner.name from event where StartDateTime>#2015-07-13T07:29:00.000Z and EndDateTime<2015-07-13T17:01:00.000Z and owner.title in ('cloud services engineer','cloud service engineer') and showas= 'busy'")
     erb :index
   end
 
