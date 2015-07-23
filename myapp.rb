f9764bf87fbfcd83463c8a87c86b7522dde769a4
vickielawlor <112431052@umail.ucc.ie>
2015-07-23 15:20:16 +0100

    Update myapp.rb

--- myapp.rb
+++ myapp.rb
@@ -55,14 +55,6 @@ class MyApp < Sinatra::Base
     end
 
   end
-  def new 
-    @student = Student.new
-  end
-  def create
-    @student = Student.new (params[:student])
-    if @student.save
-      redirect_to new_student_path
-  end
   
   
   date = Time.now
